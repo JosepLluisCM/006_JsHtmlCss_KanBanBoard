3842b3b24f1748d4db84ae6708bf62491a9359d0
JosepLluisCM <webdevJosep@gmail.com>
2023-05-29 13:25:37 +0200

    fixed overflow problem, clean files and ready for more js

--- scripts/general.js
+++ scripts/general.js
@@ -16,12 +16,12 @@ function renderBoard() {
       <div class="list-container">
         <div class="list-content">
           <div class="list-title">${list.title}</div>
-          <div class="list-cards js-list-cards">
+          <div class="list-cards">
             
           </div>
           <div class="card-creator">
-            <input type="text" class="newCard js-newCard" placeholder="Card Content" data-list-title="${list.title}">
-            <button class="card-creator-button js-card-creator-button" data-list-title="${list.title}">Add Card</button>
+            <input type="text" class="newCard" placeholder="Card Content" data-list-title="${list.title}">
+            <button class="card-creator-button" data-list-title="${list.title}">Add Card</button>
           </div>
         </div>
       </div>
@@ -29,34 +29,34 @@ function renderBoard() {
     listsHTML += HTML;
   });
 
-  document.querySelector('.js-lists-array').innerHTML = listsHTML;
+  document.querySelector('#lists-array').innerHTML = listsHTML;
 }
 
 /*__________CONTROLLER__________*/
 
 //EVENT LISTENERS
-document.querySelector('.js-list-creator-button').addEventListener('click', () => {
+document.querySelector('.list-creator-button').addEventListener('click', () => {
   
-  let listInput = document.querySelector('.js-newList').value;
+  let listInput = document.querySelector('.newList').value;
   if (listInput.length) {
     console.log(listInput);
     addList(listInput);
-    document.querySelector('.js-newList').value = '';
+    document.querySelector('.newList').value = '';
   };
 });
 
 
 
-document.querySelector('.js-board').addEventListener('click', (event) => {
-  if (event.target.classList.contains('js-card-creator-button')) {
+document.querySelector('.board').addEventListener('click', (event) => {
+  if (event.target.classList.contains('card-creator-button')) {
     //pick the id from the button pressed
     const listId = event.target.dataset.listTitle;
     //console.log(event.target.dataset); 
 
-    const cardInput = document.querySelector('.js-newCard').value;
+    const cardInput = document.querySelector('.newCard').value;
     if (cardInput.length) {
       addCard(cardInput, listId);
-      document.querySelector('.js-newCard').value = '';
+      document.querySelector('.newCard').value = '';
       console.log(cardInput + ',' + typeof listId);
     };
     
